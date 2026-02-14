d39dd9733fe104fd4fa5477427e8cd5c9ec7af71
Shantanu Digambar Dahitule <79780881+shantanu-dahitule@users.noreply.github.com>
2026-02-14 12:39:36 +0530

    Index folder

---
+++ fastvecdb/index/__init__.py
@@ -0,0 +1,7 @@
+"""Vector indexing and query routing."""
+
+from fastvecdb.index.bucket import VectorBucket
+from fastvecdb.index.router import QueryRouter
+
+__all__ = ["VectorBucket", "QueryRouter"]
+
